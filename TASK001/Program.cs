5d55afeab39c857ff15eb3ad36ea64b3d673df18
Marina Artemyeva <martemyeva@yahoo.com>
2023-02-23 12:08:30 +0600

    Add code in TASK001

--- TASK001/Program.cs
+++ TASK001/Program.cs
@@ -3,3 +3,20 @@
 //Math.Pow использовать нельзя
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
+
+int Degree(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i = 1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    return result;
+}
+  Console.Write("Введите число A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+
+  int degree = Degree(numberA, numberB);
+  Console.WriteLine("Ответ: " + degree);
