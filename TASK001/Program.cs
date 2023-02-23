db0c4ae064b9e145674c0e0f2de8c123882e219e
Marina Artemyeva <martemyeva@yahoo.com>
2023-02-23 11:40:25 +0600

    Add condition in  TASK001

--- TASK001/Program.cs
+++ TASK001/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите метод, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+//Math.Pow использовать нельзя
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
